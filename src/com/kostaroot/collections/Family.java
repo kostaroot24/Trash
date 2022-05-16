0b4015c817e6f79bf54b90153e8e85d1b7c75364
costaroot <dmytrokostiukevych92@gmail.com>
2022-05-16 20:48:31 +0300

    Family class was finished

--- src/com/kostaroot/collections/Family.java
+++ src/com/kostaroot/collections/Family.java
@@ -1,5 +1,7 @@
 package com.kostaroot.collections;
 
+import java.util.Objects;
+
 public class Family implements Comparable<Family>{
     private String name;
     private int familyMemberNumber;
@@ -44,6 +46,31 @@ public class Family implements Comparable<Family>{
     public void setNameOfEmblem(String nameOfEmblem) {
         this.nameOfEmblem = nameOfEmblem;
     }
-    
-}
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Family family = (Family) o;
+        return familyMemberNumber == family.familyMemberNumber && name.equals(family.name) && title.equals(family.title) && nameOfEmblem.equals(family.nameOfEmblem);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, familyMemberNumber, title, nameOfEmblem);
+    }
+
+    @Override
+    public String toString() {
+        return "Family " +
+                "name: " + name  +
+                "\n familyMemberNumber: " + familyMemberNumber +
+                "\n title: " + title +
+                "\n nameOfEmblem: " + nameOfEmblem;
+    }
+
+    @Override
+    public int compareTo(Family family) {
+        return 0;
+    }
+}
